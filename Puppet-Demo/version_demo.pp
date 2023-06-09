6eda58b570d325c3f6926d82b2632aa12efee842
Valerie Braun <valeriekristinea@gmail.com>
2023-06-09 08:10:38 -0500

    content for Brandon

--- Puppet-Demo/version_demo.pp
+++ Puppet-Demo/version_demo.pp
@@ -4,6 +4,6 @@ node default {
     file { 'indexPage':
                path => '/var/www/html/index.html',
                ensure => 'file',
-               source => 'https://raw.githubusercontent.com/skillstorm-congo/AWS-DevOps-StarterKit/main/DevOps/Puppet-Demo/websitev1.html?token=GHSAT0AAAAAAB7BNBXKHDB5YTUW3N774WNAZECSB7Q',
+               source => 'https://raw.githubusercontent.com/valeriedarling/content/main/Puppet-Demo/websitev1.html',
           }
   }
